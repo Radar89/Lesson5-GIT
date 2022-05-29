a5dd2a3614f3dd4c82aa6d1d675cd22b6568b2d0
Radek Borowski <r.borowski@vp.pl>
2022-05-29 10:57:34 +0200

    add new print line

--- src/com/futurecollars/Main.java
+++ src/com/futurecollars/Main.java
@@ -3,5 +3,6 @@ package com.futurecollars;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, I'm on GitHub");
+        System.out.println("new line added");
     }
 }
